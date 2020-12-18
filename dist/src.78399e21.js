7d80fc87c91502dc26de8582a526c49c3be89f87
Isaac <isaacgc08@gmail.com>
2020-12-17 16:51:43 -0800

    test heorku

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -53361,7 +53361,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39873" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45043" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
